0d222bde8254d398b71e30d39bce05ac59dcf378
d4211n9 <d4211n9@gmail.com>
2024-09-26 10:27:31 +0200

    Added missing field 'TotalPages'

--- server/data-access/data-transfer-objects/PaginationDto.cs
+++ server/data-access/data-transfer-objects/PaginationDto.cs
@@ -2,6 +2,7 @@ namespace data_access.data_transfer_objects;
 
 public class PaginationDto
 {
-    private int PageNumber { get; set; } = 0;
+    private int PageNumber { get; set; } = 1;
     private int PageSize { get; set; } = 10;
+    private int TotalPages { get; set; } = 1;
 }
