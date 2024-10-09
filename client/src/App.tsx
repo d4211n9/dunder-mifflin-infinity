0f378c24780e58961a4ba00443b717c227745e9d
d4211n9 <d4211n9@gmail.com>
2024-10-09 13:48:26 +0200

    Added user story: As a customer I want to have a product overview with filtering, ordering and full-text search preferences

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import OrderHistoryComponent from "./components/OrderHistoryComponent.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import PaperOverviewComponent from "./components/PaperOverviewComponent.tsx";
 
 function App() {
     return <>
         <BrowserRouter>
             <Routes>
                 <Route path='/orderHistory/:customerId/:pageNumber/:pageSize' element={<OrderHistoryComponent />}/>
+                <Route path='/paperOverview' element={<PaperOverviewComponent />}/>
             </Routes>
         </BrowserRouter>
     </>;
