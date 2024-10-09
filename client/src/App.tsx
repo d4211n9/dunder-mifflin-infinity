735705dced0c29869f76cd8a673fa098d4015190
d4211n9 <d4211n9@gmail.com>
2024-10-09 16:01:36 +0200

    Added create new paper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import OrderHistoryComponent from "./components/OrderHistoryComponent.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import PaperOverviewComponent from "./components/PaperOverviewComponent.tsx";
+import CreateNewPaper from "./components/CreateNewPaper.tsx";
 
 function App() {
     return <>
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path='/orderHistory/:customerId' element={<OrderHistoryComponent />}/>
                 <Route path='/paperOverview' element={<PaperOverviewComponent />}/>
+                <Route path='/paper/create' element={<CreateNewPaper />}/>
             </Routes>
         </BrowserRouter>
     </>;
